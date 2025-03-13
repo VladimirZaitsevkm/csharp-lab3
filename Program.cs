a2b6f23452e77d4d643f0123a1b9b5afd822caf7
VladimirZaitsevkm <vladimir.zaitsevvv@gmail.com>
2025-03-13 13:49:25 +0300

    Старт программы

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCSharp3PrototypePattern
+{
+  internal class Program
+  {
+
+    public class SquareMatrix
+    {
+      public int[,] matrix;
+      public int Size;
+    }
+    static void Main(string[] args)
+    {
+    }
+  }
+}
