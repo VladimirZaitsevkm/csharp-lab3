e8ea7085ffcb152e59edc9ad8669861e29f08fa4
VladimirZaitsevkm <vladimir.zaitsevvv@gmail.com>
2025-03-13 14:02:41 +0300

    метод создания матрицы + создание пользовательского класса исключения

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,23 @@ namespace LabCSharp3PrototypePattern
 
   public class SquareMatrix
   {
+    public static Random rnd = new Random();
     public int[,] matrix;
     public int Size;
+
+    public SquareMatrix(int size)
+    {
+      if (size <= 0) throw new MatrixSizeException("Размер матрицы был указан некорректно");
+      this.Size = size;
+      this.matrix = new int[size, size];
+    }
   }
+
+  public class MatrixSizeException : Exception
+  {
+    public MatrixSizeException(string message) : base(message) { }
+  }
+
   internal class Program
   {
     static void Main(string[] args)
